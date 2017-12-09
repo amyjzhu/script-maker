267875e57cc318e11583899947139a686c49e785
amyjzhu <ajyzhu@gmail.com>
2017-12-08 22:38:27 -0800

    More parsing basics

--- main.js
+++ main.js
@@ -1,19 +1,42 @@
 var $ = require("jquery");
 $(document).ready(function () {
+    $("#save").click(function () {
+        parse();
+    });
 });
 function save() {
-    var title = $("#current-event--title").value;
-    var desc = $("#current-event--description").value;
-    var scriptText = $("#current-event--script").value;
-    var character = $("#current-event--character").value;
+}
+function parse() {
+    var title = $("#current-event--title").val();
+    var desc = $("#current-event--description").val();
+    var scriptText = $("#current-event--script").val();
+    var character = $("#current-event--character").val();
+    var priority = $("#current-event--priority").val();
+    var type = $("#current-event--type").val();
+    var prerequisites = $("#current-event--prerequisites").val();
     var choices = getChoices();
+    var event = {
+        "title": title,
+        "description": desc,
+        "script": scriptText,
+        "character": character,
+        "priority": priority,
+        "type": type,
+        "prerequisites": prerequisites,
+        "choices": choices
+    };
+    $("#result").html(event);
+    console.log(event);
 }
 function getChoices() {
     var choices = [];
     for (var i = 0; i < 3; i++) {
-        var choiceString = "#current-event--choice" + i;
-        var choice = $(choiceString + "-category").value;
-        var goTo = $(choiceString + "-result").value;
+        var choiceString = "#current-event--choice" + i + 1;
+        var choice = $(choiceString + "-category").val();
+        var goTo = $(choiceString + "-result").val();
+        choices[i] = { choice: goTo };
     }
     return choices;
+}
+function parseScript() {
 }
--- main.ts
+++ main.ts
@@ -1,28 +1,58 @@
 var $ = require("jquery");
 
 $(document).ready(function() {
+
+    $("#save").click(function() {
+        parse();
+    })
+
     }
-)
+);
+
 
 
 function save() : void {
-    let title : string = $("#current-event--title").value;
-    let desc : string = $("#current-event--description").value;
-    let scriptText : string = $("#current-event--script").value;
-    let character = $("#current-event--character").value;
+
+}
+
+function parse() : void {
+    let title : string = $("#current-event--title").val();
+    let desc : string = $("#current-event--description").val();
+    let scriptText : string = $("#current-event--script").val();
+    let character = $("#current-event--character").val();
+    let priority = $("#current-event--priority").val();
+    let type = $("#current-event--type").val();
+    let prerequisites = $("#current-event--prerequisites").val();
     let choices = getChoices();
 
+    let event = {
+        "title": title,
+        "description": desc,
+        "script": scriptText,
+        "character": character,
+        "priority":priority,
+        "type": type,
+        "prerequisites" :prerequisites,
+        "choices": choices
+    };
 
+    $("#result").html(event);
+    console.log(event)
 }
 
 function getChoices() : any[] {
     let choices = [];
 
     for (let i = 0; i < 3; i++) {
-        let choiceString = "#current-event--choice" + i;
-        let choice = $(choiceString + "-category").value;
-        let goTo = $(choiceString + "-result").value;
+        let choiceString = "#current-event--choice" + i+1;
+        let choice = $(choiceString + "-category").val();
+        let goTo = $(choiceString + "-result").val();
+        choices[i] = {choice: goTo};
     }
 
-    return choices
+    return choices;
+}
+
+function parseScript() {
+
 }
